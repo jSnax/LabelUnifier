2fcf72abcd5b5268baedb14c22bc173321f9fed7
Sebastian Groß <sgross91@uni-koblenz.de>
2018-05-16 14:28:58 +0200

    updating main

--- src/main/java/de/uni_koblenz/ppbks18/MainPP.java
+++ src/main/java/de/uni_koblenz/ppbks18/MainPP.java
@@ -1,8 +1,22 @@
 package de.uni_koblenz.ppbks18;
 
+import java.io.IOException;
+
+import de.uni_koblenz.label.Label;
+import net.sf.extjwnl.JWNLException;
+
 
 public class MainPP {
-	public static void main (String args[]) {
-		System.out.println("Hello World");
+	
+	public static void main (String args[]) throws JWNLException, ClassNotFoundException, IOException {
+	
+		Label l1 = new Label();
+		Label l2 = new Label();
+		Label l3 = new Label();
+		
+		l1.tagLabel("checking invoice");
+		l2.tagLabel("invoice checked");
+		l3.tagLabel("This is a really short sentence");
+
 	}
 }
