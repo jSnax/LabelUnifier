0fbe9cb43bf850f4d92f5bc6f48a46b00a778064
Alex Fredekind <alexfredekind@uni-koblenz.de>
2018-05-20 19:18:26 +0200

    delete interface, add "Hello"

--- src/main/java/de/uni_koblenz/ppbks18/IWordnetLibrary.java
+++
@@ -1,9 +0,0 @@
-package de.uni_koblenz.ppbks18;
-
-import rita.*;
-
-//implementiert ein Interface, um die RiTa library global zu verwenden
-
-public interface IWordnetLibrary {
-	public static RiWordNet wordnet = new RiWordNet ("<Pfad einfügen>");
-}
