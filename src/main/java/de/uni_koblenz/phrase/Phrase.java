8e17f5476da8c68e684fe1bd76fb20903e1e5b89
Monika <Monika@192.168.0.192>
2018-05-31 01:57:14 +0200

    My first commit.  Added Jaccard and Vector similarity + beginning of Phrase Structure application.

--- src/main/java/de/uni_koblenz/phrase/Phrase.java
+++ src/main/java/de/uni_koblenz/phrase/Phrase.java
@@ -1,6 +1,9 @@
 package de.uni_koblenz.phrase;
-
+import java.util.ArrayList;
+import java.util.List;
 import de.uni_koblenz.label.*;
+import de.uni_koblenz.enums.*;
+
 
 public class Phrase {
 
@@ -26,7 +29,43 @@ public class Phrase {
 	public void setWordsArray(Word[] wordsArray) {
 		this.wordsArray = wordsArray;
 	}
+	public String extractPhraseStructure (String Namingconventions){
+		String PhraseStructure = "";
+		
+		return PhraseStructure;
+	}
+	
+	public List<String> findApplicablePS(List<String> lbls, List<List<String>> phraseStructures){
+		List<String> resPS = new ArrayList<String>();
+		//if Action/State
+		// finding out which type of PhraseStructures to use
+		for(int i = 0; i < phraseStructures.size(); i++){
+			if (lbls.size() == phraseStructures.size()){
+				resPS = phraseStructures.get(i);
+			}
+		}
+		return resPS;
+	}
 	
 	
-	
+	public boolean fulfillsPhraseStructure(String wordCluster, List<String> lbls, List<String> phraseStructure){
+		boolean correct = true;
+		int labelIndex = 1;
+		int phraseIndex = 1;
+		while(labelIndex < lbls.size()){
+			String phrasePOS = phraseStructure.get(phraseIndex);
+			String labelPOS = ""; 
+//			labelPOS = getPartOfSpeech(lbls.get(labelIndex));
+			if(phrasePOS != labelPOS){
+				correct = false;
+				break;
+			}
+			else {
+				labelIndex++;
+				phraseIndex++;
+			}
+		}
+		return correct;
+	}	
 }
+
