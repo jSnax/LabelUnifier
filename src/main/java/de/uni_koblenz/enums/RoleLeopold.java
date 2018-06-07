578dabee272865214a039eabbe37fd6a95bdb299
Carsten Knuffmann <cknuffmann@uni-koblenz.de>
2018-06-07 12:04:56 +0200

    added weightedValue to RoleLeopold

--- src/main/java/de/uni_koblenz/enums/RoleLeopold.java
+++ src/main/java/de/uni_koblenz/enums/RoleLeopold.java
@@ -1,5 +1,27 @@
 package de.uni_koblenz.enums;
 
 public enum RoleLeopold {
-	ACTION, BUSINESS_OBJECT, OPTIONAL_INFORMATION_FRAGMENT, SUBJECT
+	ACTION(1.0), 
+	BUSINESS_OBJECT(1.0), 
+	OPTIONAL_INFORMATION_FRAGMENT(0.5), 
+	SUBJECT(0.5);
+
+private double weightedValue;
+
+private RoleLeopold() {
+	
+}
+
+private RoleLeopold(double weightedValue){
+	this.weightedValue = weightedValue;
+}
+
+public double getWeightedValue() {
+	return weightedValue;
+}
+
+public void setWeightedValue(double weightedValue) {
+	this.weightedValue = weightedValue;
+}
+
 }
