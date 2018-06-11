4a873b87cd1cdd2e606a8d8693392faa855b92f0
Tim Schneichel <tschneichel@uni-koblenz.de>
2018-06-11 17:26:32 +0200

    Added Passive and Present Participle, changed some determiners

--- src/main/java/de/uni_koblenz/enums/PhraseStructureTypes.java
+++ src/main/java/de/uni_koblenz/enums/PhraseStructureTypes.java
@@ -3,13 +3,15 @@ package de.uni_koblenz.enums;
 public enum PhraseStructureTypes {
 	VERB_IMPERATIVE("Verb"),
 	VERB_BASE("Verb"),
+	VERB_PASSIVE("Verb"),
+	VERB_PRESENT_PARTICIPLE("Verb"),
 	VERB_INDICATIVE("Verb"),
 	VERB_SIMPLEPRESENT("Verb"),
 	VERB_SIMPLEPAST("Verb"),
-	NOUN_SINGULAR_OBJECT("Noun"),
-	NOUN_SINGULAR_SUBJECT("Noun"),
-	NOUN_PLULAR_OBJECT("Noun"),
-	NOUN_PLULAR_SUBJECT("Noun"),
+	NOUN_SINGULAR_OBJECT("Object"),
+	NOUN_SINGULAR_SUBJECT("Subject"),
+	NOUN_PLULAR_OBJECT("Object"),
+	NOUN_PLULAR_SUBJECT("Subject"),
 	DETERMINER_DEFINITEARTICLE("Determiner"),
 	DETERMINER_INDEFINITEARTICLE("Determiner"),
 	PUNCTUATION_PERIOD("Punctuation"),
@@ -20,5 +22,8 @@ public enum PhraseStructureTypes {
 	
 	private PhraseStructureTypes (String determiner) {
 		this.determiner = determiner;
+	}
+	public String getdeterminer() {
+		return this.determiner;
 	}
 }
