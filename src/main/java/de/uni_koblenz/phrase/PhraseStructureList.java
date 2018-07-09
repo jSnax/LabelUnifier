db946adfb671740bbb3ccf97861abc01fa8c9a53
Tim Schneichel <tschneichel@uni-koblenz.de>
2018-07-09 18:14:48 +0200

    new class, stores only a list of phraseStructures

---
+++ src/main/java/de/uni_koblenz/phrase/PhraseStructureList.java
@@ -0,0 +1,27 @@
+package de.uni_koblenz.phrase;
+
+import java.util.ArrayList;
+import java.util.List;
+//<<<<<<< Updated upstream
+//=======
+import java.util.regex.Pattern;
+//>>>>>>> Stashed changes
+import java.util.regex.Matcher;
+
+import de.uni_koblenz.enums.PartOfSpeechTypes;
+import de.uni_koblenz.enums.PhraseStructureTypes;
+
+import java.io.IOException;
+
+public class PhraseStructureList {
+	List<PhraseStructure> AllStructures;
+	
+	public List<PhraseStructure> getAllStructures() {
+		return AllStructures;
+	}
+
+	public void setAllStructures(List<PhraseStructure> allStructures) {
+		this.AllStructures = allStructures;
+	}
+
+}
