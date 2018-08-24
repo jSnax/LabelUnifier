b8466b44e5dc7feec6ec811cbe28dedb665408a5
Carsten Knuffmann <cknuffmann@uni-koblenz.de>
2018-08-24 09:41:44 +0200

    Continued work on PhraseStructureList

--- src/main/java/de/uni_koblenz/phrase/PhraseStructureList.java
+++ src/main/java/de/uni_koblenz/phrase/PhraseStructureList.java
@@ -39,23 +39,75 @@ public class PhraseStructureList {
         	}
         	
         	else if(phraseElement[0] == "y") {
-        		PhraseStructure structure = new PhraseStructure();
+        		PhraseStructure Structure = new PhraseStructure();
+        		List<PhraseStructureTypes> tempList = new ArrayList<PhraseStructureTypes>();
+        		//Ein Tab == 5 Chars (bzw 5 Leerzeichen)
+        		br.skip(5);
         		for(int i = 1; i < line.length(); i++) {
-        			// Hier komm ich grad nicht weiter.
-        			/*pstypes = null
+        			String PhraseString = phraseElement[i];
+        			// Man kann nicht über einen String switchen...
+        			pstypes = Structure.getElements().get(i);
         			switch(pstypes) {
-        			case VERB_BASE: 
-        				structure.setPhraseElements(phraseElement[i]);
+        			case VERB_IMPERATIVE: 
+        				tempList.add(PhraseStructureTypes.VERB_IMPERATIVE);
         				break;
-        				
+        			case VERB_BASE: 
+        				tempList.add(PhraseStructureTypes.VERB_BASE);
+        				break;	
+        			case VERB_PASSIVE: 
+        				tempList.add(PhraseStructureTypes.VERB_PASSIVE);
+        				break;
+        			case VERB_PASSIVE_PAST: 
+        				tempList.add(PhraseStructureTypes.VERB_PASSIVE_PAST);
+        				break;
+        			case VERB_PRESENT_PARTICIPLE: 
+        				tempList.add(PhraseStructureTypes.VERB_PRESENT_PARTICIPLE);
+        				break;
+        			case VERB_SIMPLEPAST: 
+        				tempList.add(PhraseStructureTypes.VERB_SIMPLEPAST);
+        				break;
+        			case NOUN_SINGULAR_OBJECT: 
+        				tempList.add(PhraseStructureTypes.NOUN_SINGULAR_OBJECT);
+        				break;
+        			case NOUN_SINGULAR_SUBJECT: 
+        				tempList.add(PhraseStructureTypes.NOUN_SINGULAR_SUBJECT);
+        				break;
+        			case NOUN_PLURAL_OBJECT: 
+        				tempList.add(PhraseStructureTypes.NOUN_PLURAL_OBJECT);
+        				break;
+        			case NOUN_PLURAL_SUBJECT: 
+        				tempList.add(PhraseStructureTypes.NOUN_PLURAL_SUBJECT);
+        				break;
+        			case DETERMINER_DEFINITEARTICLE: 
+        				tempList.add(PhraseStructureTypes.DETERMINER_DEFINITEARTICLE);
+        				break;
+        			case DETERMINER_INDEFINITEARTICLE: 
+        				tempList.add(PhraseStructureTypes.DETERMINER_INDEFINITEARTICLE);
+        				break;
+        			case PUNCTUATION_PERIOD: 
+        				tempList.add(PhraseStructureTypes.PUNCTUATION_PERIOD);
+        				break;
+        			case PUNCTUATION_QUESTIONMARK: 
+        				tempList.add(PhraseStructureTypes.PUNCTUATION_QUESTIONMARK);
+        				break;
+        			case ADJECTIVE_FOR_OBJECT: 
+        				tempList.add(PhraseStructureTypes.ADJECTIVE_FOR_OBJECT);
+        				break;
+        			case ADJECTIVE_FOR_SUBJECT: 
+        				tempList.add(PhraseStructureTypes.ADJECTIVE_FOR_SUBJECT);
+        				break;
+        			case ADVERB: 
+        				tempList.add(PhraseStructureTypes.ADVERB);
+        				break;
+        			default: System.out.println("No valid PhraseStructureType");
+        		}
         			
-        			
-        			default:
-        		}*/
+        		br.skip(5);
         	}
         	
+        	//tempList Ergebnis WO speichern?
         	List<PhraseStructure> tsvlist = new ArrayList<PhraseStructure>();
-        	tsvlist.add(structure);
+        	
         		
         	}
         	
