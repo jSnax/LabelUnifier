171cf8db2c61bfbf97b601cc597d9e8fcf0c10ee
Jan Angemeer <jangemeer@uni-koblenz.de>
2018-08-28 10:11:25 +0200

    Tests for reading Labels & IDs from pnml files

--- src/test/java/ReadModels.java
+++ src/test/java/ReadModels.java
@@ -1,42 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class ReadModels {
-	public ArrayList <String> GetIDs (File y) throws FileNotFoundException {
-		Scanner sc;
-		ArrayList <String> ID = new ArrayList <String>();
-		if(y.exists()) {
-			 sc = new Scanner(y);
-			while (sc.hasNextLine()) {
-				if (sc.hasNext("<place") ) {
-					sc.next();
-					ID.add(sc.next());}	
-				else 
-					sc.nextLine();
-			}
-		}
-		return ID ;
-	}
-	
-	public ArrayList <String> ReadLabels (File y) throws FileNotFoundException{
-		Scanner sc;
-		ArrayList <String> LabelList = new ArrayList <String>();
-		if(y.exists()) {
-			 sc = new Scanner(y);
-			 String x = "";
-			while (sc.hasNextLine()) {
-				if (sc.hasNext("<place") ) {
-					while (!sc.hasNext("</place>") ) {
-						x = x + sc.nextLine();
-						}
-					LabelList.add(x);}
-				else 
-					sc.nextLine();
-			}
-		}
-		return LabelList;
-	}
 
 }
--- src/test/java/ReadModelsTest.java
+++ src/test/java/ReadModelsTest.java
@@ -1,21 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
 public class ReadModelsTest {
-	public static void main(String[] args) throws FileNotFoundException {
-		System.out.println("Algorithmus gestarted");
-		ReadModels Reader = new ReadModels() ;
-		ArrayList <String> IDs = new ArrayList <String> ();
-		ArrayList <String> Labelliste = new ArrayList <String> ();
-		File Model = new File("birthCertificate_p31");
-		// not done yet
-		IDs= Reader.GetIDs(Model);
-		Labelliste = Reader.ReadLabels(Model);
-		System.out.println(IDs);
-		System.out.println(Labelliste);
-		System.out.println("Algorithmus beendet");
-	}
-}	
 
-
+}
