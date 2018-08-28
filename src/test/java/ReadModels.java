bf508ce4884ce2a5b249dbd6a00d1faeec28eb69
Jan Angemeer <jangemeer@uni-koblenz.de>
2018-08-28 11:38:41 +0200

    Update: Test classes for reading labels from pnml files

--- src/test/java/ReadModels.java
+++ src/test/java/ReadModels.java
@@ -1,4 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ReadModels {
+	public ArrayList <String> GetIDs (File y) throws FileNotFoundException {
+		Scanner sc;
+		ArrayList <String> ID = new ArrayList <String>();
+		if(y.exists()) {
+			 sc = new Scanner(y);
+			while (sc.hasNextLine()) {
+				if (sc.hasNext("<place") || sc.hasNext("<transition") ) {
+					sc.next();
+					ID.add(sc.next());}	
+				else 
+					sc.nextLine();
+			}
+		}
+		return ID ;
+	}
+	
+	public ArrayList <String> ReadLabels (File y) throws FileNotFoundException{
+		Scanner sc;
+		ArrayList <String> LabelList = new ArrayList <String>();
+		if(y.exists()) {
+			 sc = new Scanner(y);
+			while (sc.hasNextLine()) {
+				if (sc.hasNext("<place") || sc.hasNext("<transition")) {
+					sc.nextLine();
+					sc.nextLine();
+						LabelList.add(sc.nextLine());}
+					else 
+						sc.nextLine();
+			}}
+		
+		return LabelList;
+	}
 
 }
--- src/test/java/ReadModelsTest.java
+++ src/test/java/ReadModelsTest.java
@@ -1,4 +1,24 @@
 
-public class ReadModelsTest {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
-}
+
+public class ReadModelsTest {
+	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println("Algorithmus gestarted");
+		ReadModels Reader = new ReadModels() ;
+		ArrayList <String> IDs = new ArrayList <String> ();
+		ArrayList <String> Labelliste = new ArrayList <String> ();
+		File Model = new File("C:\\Users\\Jan Angemeer\\git\\SemanticProcessModeling\\src\\test\\resources\\dataset2\\models\\birthCertificate_p246.pnml");
+		// not done yet
+		
+		IDs= Reader.GetIDs(Model);
+		System.out.println("Die IDs des Models sind:" + IDs);
+		
+		Labelliste = Reader.ReadLabels(Model);
+		System.out.println("Die Labels des Models sind:" + Labelliste);
+		System.out.println("Algorithmus beendet");
+	}
+}	
+
