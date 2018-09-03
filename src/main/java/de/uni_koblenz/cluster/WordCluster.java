471822fa936c9c7916ee6dd2cf5a1c4f2f95e567
Tim Schneichel <tschneichel@uni-koblenz.de>
2018-09-03 19:43:13 +0200

    Included domainThesaurus in calculateDominance

--- src/main/java/de/uni_koblenz/cluster/WordCluster.java
+++ src/main/java/de/uni_koblenz/cluster/WordCluster.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import de.uni_koblenz.label.*;
+import de.uni_koblenz.phrase.DomainThesaurus;
 
 public class WordCluster {
 
@@ -31,33 +32,48 @@ public class WordCluster {
 		this.matchingWords = matchingWords;
 	}
 	
-	// TODO: Übergabe eines Domänenthesaurus, der Dominanz vorgibt zu einzelnen Worten
-	public void calculateDominance(){
-		for (int i = 0; i < this.matchingWords.size(); i++){
-			 Word tempWord = this.matchingWords.get(i);
-			 tempWord.setDominance(0);
-		}
-		// Sets dominance for all Words to 0 at first to avoid NullPointerException
-		for (int i = 0; i < this.matchingWords.size(); i++){
-			if (!(this.matchingWords.get(i).getDominance() > 0)){
-				 Word tempWord = this.matchingWords.get(i);
-			     List<Integer> tempIntList = new ArrayList<Integer>();
-			     int counter = 1;
-				 for (int j = i; j < this.matchingWords.size(); j++){
-					 if (this.matchingWords.get(j).getBaseform().equals(tempWord.getBaseform())){
-						 counter++;
-						 tempIntList.add(j);
-					 }
-				 }
-				 this.matchingWords.get(i).setDominance(counter);
-				 for (int j = tempIntList.size() - 1; j >= 0 ; j--){
-					 this.matchingWords.get(j).setDominance(counter);
-					 tempIntList.remove(j);
-				 }
+	public void calculateDominance(DomainThesaurus thesaurus){
+		boolean matchFound;
+		boolean skipRest = false;
+		int thesaurusCounter;
+		for (int clusterCounter = 0; clusterCounter < this.matchingWords.size(); clusterCounter++){	
+			matchFound = false;
+			thesaurusCounter = 0;
+			Word tempWord = this.matchingWords.get(clusterCounter);
+			while (!(matchFound) && thesaurusCounter < thesaurus.getAllWords().size()){
+				if (this.matchingWords.get(clusterCounter).getBaseform().equals(thesaurus.getAllWords().get(thesaurusCounter))){
+					tempWord.setDominance(2147483647);
+					matchFound = true;
+					skipRest = true;
+				}
+				thesaurusCounter++;
 			}
-			// Calculates Dominance for each Word in the Cluster
-			else 
-				continue;
+			// As soon as a word in the cluster is also in the DomainThesaurus, its dominance is set to max int and the while loop ends
+			 if (!(matchFound)) tempWord.setDominance(0);
+		}
+		// Sets dominance for all other Words to 0 at first to avoid NullPointerException
+		if (!(skipRest)){
+			for (int i = 0; i < this.matchingWords.size(); i++){
+				if (!(this.matchingWords.get(i).getDominance() > 0)){
+					 Word tempWord = this.matchingWords.get(i);
+				     List<Integer> tempIntList = new ArrayList<Integer>();
+				     int counter = 1;
+					 for (int j = i; j < this.matchingWords.size(); j++){
+						 if (this.matchingWords.get(j).getBaseform().equals(tempWord.getBaseform())){
+							 counter++;
+							 tempIntList.add(j);
+						 }
+					 }
+					 this.matchingWords.get(i).setDominance(counter);
+					 for (int j = tempIntList.size() - 1; j >= 0 ; j--){
+						 this.matchingWords.get(j).setDominance(counter);
+						 tempIntList.remove(j);
+					 }
+				}
+				// Calculates Dominance for each Word in the Cluster
+				else 
+					continue;
+			}
 		}
 	}
 	
